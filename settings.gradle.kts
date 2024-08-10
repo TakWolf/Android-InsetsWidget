22150a1d6a60d8c5d4fa53327d20f409c17fd2e2
TakWolf <takwolf@foxmail.com>
2024-08-11 05:46:30 +0800

    升级构建

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,6 +16,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Android-InsetsWidget"
 
-include(":insetswidget")
-include(":constraintlayout")
-include(":app")
+include("insetswidget")
+include("constraintlayout")
+include("app")
