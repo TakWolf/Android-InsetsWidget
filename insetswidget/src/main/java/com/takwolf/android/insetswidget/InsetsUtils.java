df72f5363470d0170decc8b7535433d8015cbc9d
TakWolf <takwolf@foxmail.com>
2025-11-21 19:15:28 +0800

    修复 insets 枚举

--- insetswidget/src/main/java/com/takwolf/android/insetswidget/InsetsUtils.java
+++ insetswidget/src/main/java/com/takwolf/android/insetswidget/InsetsUtils.java
@@ -2,13 +2,14 @@ package com.takwolf.android.insetswidget;
 
 import androidx.annotation.NonNull;
 import androidx.core.graphics.Insets;
+import androidx.core.view.HackWindowInsetsCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 final class InsetsUtils {
     private InsetsUtils() {}
 
     private static final int INSETS_TYPE_FIRST = InsetsWidget.INSETS_TYPE_STATUS_BARS;
-    private static final int INSETS_TYPE_LAST = InsetsWidget.INSETS_TYPE_DISPLAY_CUTOUT;
+    private static final int INSETS_TYPE_LAST = InsetsWidget.INSETS_TYPE_SYSTEM_OVERLAYS;
 
     @NonNull
     static Insets getInsets(@NonNull WindowInsetsCompat windowInsets, @InsetsWidget.InsetsType int insetsType) {
@@ -39,6 +40,12 @@ final class InsetsUtils {
                         break;
                     case InsetsWidget.INSETS_TYPE_DISPLAY_CUTOUT:
                         result |= WindowInsetsCompat.Type.displayCutout();
+                        break;
+                    case InsetsWidget.INSETS_TYPE_WINDOW_DECOR:
+                        result |= HackWindowInsetsCompat.Type.windowDecor();
+                        break;
+                    case InsetsWidget.INSETS_TYPE_SYSTEM_OVERLAYS:
+                        result |= WindowInsetsCompat.Type.systemOverlays();
                         break;
                 }
             }
