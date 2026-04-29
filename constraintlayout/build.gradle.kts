d39fb3d3d125c48ccfe28687a67c0a937b32c5e5
TakWolf <takwolf@foxmail.com>
2026-04-30 01:09:14 +0800

    升级依赖

--- constraintlayout/build.gradle.kts
+++ constraintlayout/build.gradle.kts
@@ -7,7 +7,7 @@ android {
     namespace = "com.takwolf.android.insetswidget.constraintlayout"
     compileSdk {
         version = release(37) {
-            minorApiLevel = 0
+            minorApiLevel = 1
         }
     }
 
@@ -24,9 +24,9 @@ android {
 }
 
 dependencies {
-    compileOnly("org.jspecify:jspecify:1.0.0")
-    compileOnly("androidx.core:core-ktx:1.18.0")
-    compileOnly("androidx.constraintlayout:constraintlayout:2.2.1")
+    compileOnly("org.jspecify:jspecify:1.0.1")
+    compileOnly("androidx.core:core-ktx:1.19.0")
+    compileOnly("androidx.constraintlayout:constraintlayout:2.2.2")
     api(project(":insetswidget"))
 }
 
